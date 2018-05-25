3343f3370967acd89b059259a907aa5b0af705da
hh <huih1984@gmail.com>
2018-05-25 23:46:11 +0800

    添加遍历基数

--- python/compute_proj.py
+++ python/compute_proj.py
@@ -1,6 +1,12 @@
 import itertools
+num_arr = []
+for ind in range(4):
+    num_arr.append(list(range(ind,16,4)))
+for ind in range(4):
+    num_arr.append(list(range(ind*4,(ind+1)*4)))
 
-num_arr = list(range(1,9))
+num_arr.append(list(range(0,16,5)))
+num_arr.append([3,7,10,12])
 print(num_arr)
 permu = list(itertools.permutations(num_arr,8))
 print(permu)
